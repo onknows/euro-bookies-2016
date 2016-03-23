2d1f95f0ec15aa3db4668c8c5e51e9b5b98f80f9
christophe.o.hesters <toefel18@gmail.com>
2016-03-23 21:04:40 +0100

    closes #16 added workshop server set-up in vagrant

---
+++ vm-test-cloud/get-cluster-inventory.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# retrieves the IP addresses of each machine in the test cluster
+# and stores them as an inventory format file which ansible can consume
+
+DOCKERVM_IP=$(vagrant ssh dockervm -c "ip address show eth1 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/\/.*$//'")
+DB_IP=$(vagrant ssh mysql -c "ip address show eth1 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/\/.*$//'")
+BUILDSERVER_IP=$(vagrant ssh buildserver -c "ip address show eth1 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/\/.*$//'")
+
+echo "docker node       $DOCKERVM_IP"
+echo "db node           $DB_IP"
+echo "buildserver node  $BUILDSERVER_IP"
+
+echo "creating  ../ansible/vm-test-cloud  which you can use as inventory"
+
+cat << EOF > ../ansible/vm-test-cloud
+[docker]
+$DOCKERVM_IP
+
+[db]
+$DB_IP
+
+[buildserver]
+$BUILDSERVER_IP
+
+[all:children]
+docker
+db
+buildserver
+EOF
