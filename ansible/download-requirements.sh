6c7730622eb727c8b3cbf60c8aa1cc992f55e2e6
christophe.o.hesters <toefel18@gmail.com>
2016-03-26 12:20:58 +0100

    installed docker and mysql using ansible-galaxy roles, provided a setup.sh scripts and download-requirements.sh script to automate some set-up tasks.

---
+++ ansible/download-requirements.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo ansible-galaxy install -r requirements.yml
---
+++ ansible/setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Run this script once after a checkout to set-up the environment
+
+echo "downloading Ansible roles ..."
+download-requirements.sh
+echo "creating .ssh directory ..."
+mkdir ~/.ssh/
+echo "creating a key-pair that ansible will use..."
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+echo "disabling host checking for this session ..., for new sessions use:"
+echo "export ANSIBLE_HOST_KEY_CHECKING=False"
+export ANSIBLE_HOST_KEY_CHECKING=False
+echo "installing generated key into all servers ...\n enter password for remote machine"
+ansible-playbook -i $1 install-ssh-key.yml --ask-pass
