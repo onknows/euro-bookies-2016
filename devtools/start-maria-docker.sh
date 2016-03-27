fa594eb533369f5a1f812dabbae76bc84d8f9813
christophe.o.hesters <toefel18@gmail.com>
2016-03-27 16:42:34 +0200

    #13 updated devtools to sync it more with the ansible scripts

--- devtools/start-maria-docker.sh
+++ devtools/start-maria-docker.sh
@@ -1,7 +1,7 @@
 echo "Starting a mariadb instance using docker for testing purposes"
 echo "port 3307"
 echo "user=root"
-echo "password=bookies-password"
+echo "password=root"
 
 echo "Starting up... "
 
@@ -9,7 +9,7 @@ echo "RUNNING COMMANDS INSIDE THE CONTAINER"
 echo " docker exec -it bookies-maria bash"
 echo " export TERM=\"dumb\""
 echo " mysql -u root -p"
-echo " enter bookies-password"
+echo " enter root"
 echo " run SQL"
 
 echo "STOPPING AND STARTING WITHOUT LOSING DATA"
@@ -22,5 +22,5 @@ echo " docker rm -f bookies-maria"
 
 
 docker pull mariadb:10.0.24
-docker run --name bookies-maria -e MYSQL_ROOT_PASSWORD=bookies-password -d -p 3307:3306 mariadb:10.0.24
+docker run --name bookies-maria -e MYSQL_ROOT_PASSWORD=root -p 3307:3306 mariadb:10.0.24
 
