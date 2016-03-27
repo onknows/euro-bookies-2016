396d9faff13155ea46236d4cedb7d9c01e295cce
christophe.o.hesters <toefel18@gmail.com>
2016-03-27 15:21:36 +0200

    aws-setup is now more complete

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -3,12 +3,16 @@
 
 echo "downloading Ansible roles ..."
 download-requirements.sh
+
 echo "creating .ssh directory ..."
 mkdir ~/.ssh/
+
 echo "creating a key-pair that ansible will use..."
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
 echo "disabling host checking for this session ..., for new sessions use:"
 echo "export ANSIBLE_HOST_KEY_CHECKING=False"
 export ANSIBLE_HOST_KEY_CHECKING=False
+
 echo "installing generated key into all servers ...\n enter password for remote machine"
 ansible-playbook -i $1 install-ssh-key.yml --ask-pass
--- aws-setup/aws-ansiblecc-setup.sh
+++ aws-setup/aws-ansiblecc-setup.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-# Install the workshop's public key that, use the private key with putty or openssh
+echo "creating .ssh directory ..."
+mkdir -p .ssh
+echo "Installing the workshop's public key, use the private key with putty or openssh"
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7ymBRUodtreE3trfzmhT92VqV1FlvoE60InQM1FXMBE1S30eAuUgg928LvhzStB6GSwuYzQpfMIU+WafeSj2j3azPfwUrTBlKpLnYTBc9FuC41kYUooC+nvEDUouSEgCZzZDGThtoTNB7R3bB8sc3grI5/4xi832NsHJU5ma/gd64nv2iJnOcXQcDHC60OZDJVjt1GaowypviWSv1Uchpr3holyJ7JO1WT7ZvljHqMmJp0bhnerupR1x/xRNVwNBIxOkmBZDkEcXwjypoif20sEvHr7UW0FWa0SkNF9Vs81ymkhlD5Yla94hBZhA3otQ5VX1R3zu1AE3HqculmA6V workshop-keypair" >> ~/.ssh/authorized_keys
-
-# Installs git and the latest version of Ansible
+echo "creating a key-pair that ansible will use to connect to server machines..."
+ssh-keygen -t rsa -N "" -f .ssh/id_rsa
+echo "Installs git and the latest version of Ansible"
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install git git-extras software-properties-common ansible -y
 
-# Clones the ansible branch 
+echo "Cloning euro bookies ansible branch"
 git clone https://github.com/toefel18/euro-bookies-2016.git --single-branch --branch ansible
+
+echo "disabling ansible host checking"
+echo "export ANSIBLE_HOST_KEY_CHECKING=False" >> .bashrc
