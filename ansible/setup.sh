ccd8edbc94719956e804458adc400d81d397cb6f
christophe.o.hesters <toefel18@gmail.com>
2016-03-27 15:24:28 +0200

    setup is now simpler and only does the necessary steps

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
-# Run this script once after a checkout to set-up the environment
+# Downloads external Ansible roles and installs SSH keys all the servers found in the hosts file
 
-echo "downloading Ansible roles ..."
-download-requirements.sh
+red=`tput setaf 1`
+reset=`tput sgr0`
 
-echo "creating .ssh directory ..."
-mkdir ~/.ssh/
+if [ -z "$1" ] ; then
+    echo "${red}"
+    echo "setup.sh needs 1 arugment: an inventory file"
+    echo "${reset}Example: "
+    echo "./setup.sh staging"
+else
+    echo "downloading Ansible roles ..."
+    download-requirements.sh
 
-echo "creating a key-pair that ansible will use..."
-ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
-
-echo "disabling host checking for this session ..., for new sessions use:"
-echo "export ANSIBLE_HOST_KEY_CHECKING=False"
-export ANSIBLE_HOST_KEY_CHECKING=False
-
-echo "installing generated key into all servers ...\n enter password for remote machine"
-ansible-playbook -i $1 install-ssh-key.yml --ask-pass
+    echo "installing generated key into all servers ...\n enter password for remote machine"
+    ansible-playbook -i $1 install-ssh-key.yml --ask-pass
+fi
