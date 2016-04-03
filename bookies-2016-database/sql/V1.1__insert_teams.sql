bf7335c96205d53b030bb37776e8a6af7f9359aa
Christophe Hesters <christophe.o.hesters@cgi.com>
2016-04-03 12:43:07 +0200

    #21 added automatic DB migrations using Flyway.

--- bookies-2016-database/sql/V1.1__insert_teams.sql
+++ bookies-2016-database/sql/V1.1__insert_teams.sql
@@ -1,17 +1,4 @@
--- CREATE DATABASE bookies_db;
-
-USE bookies_db;
-
-CREATE TABLE teams
-(
-  id int NOT NULL AUTO_INCREMENT,
-  teamName varchar(255) NOT NULL,
-  countryCode varchar(255) NOT NULL,
-  PRIMARY KEY(ID)
-);
-
 INSERT INTO teams (teamname, countryCode) VALUES ('Oranje', 'nl');
 INSERT INTO teams (teamname, countryCode) VALUES ('Germany', 'de');
 INSERT INTO teams (teamname, countryCode) VALUES ('Belgium', 'be');
 INSERT INTO teams (teamname, countryCode) VALUES ('Spain', 'es');
-commit;
---
+++ bookies-2016-database/sql/V1__create_teams.sql
@@ -0,0 +1,7 @@
+CREATE TABLE teams
+(
+  id int NOT NULL AUTO_INCREMENT,
+  teamName varchar(255) NOT NULL,
+  countryCode varchar(255) NOT NULL,
+  PRIMARY KEY(ID)
+);
