bde68058b1e2896d9f3e6264b6fa74341a51cceb
christophe.o.hesters <toefel18@gmail.com>
2016-05-13 23:01:17 +0200

    test script is now a lot easier

--- bookies-2016-database/start-mysql-docker.sh
+++ bookies-2016-database/start-mysql-docker.sh
@@ -1,35 +1,21 @@
 #!/usr/bin/env bash
 echo "Starting a mariadb instance using docker for testing purposes"
-echo "port 3306"
-echo "user=root"
-echo "password=root"
+docker run --name bookies-maria -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=bookies_db -e MYSQL_USER=bookies -e MYSQL_PASSWORD=bookies mariadb:10.0.24
+echo "waiting for container to be ready..."
+sleep 15
+echo "Use flyway to bring the database schema to the latest version (flyway must be on PATH)"
+flyway -user=root -password=root -url=jdbc:mysql://localhost:3306/bookies_db -locations=filesystem:sql migrate
 
-echo "Starting up... "
 
 echo "RUNNING COMMANDS INSIDE THE CONTAINER"
-echo " docker exec -it bookies-maria bash"
-echo " export TERM=\"dumb\""
-echo " mysql -u root -p"
-echo " enter root"
-echo " run SQL"
-
-echo "STOPPING AND STARTING WITHOUT LOSING DATA"
-echo " docker stop bookies-maria"
-echo " ... wait till next day ... "
-echo " docker stop bookies-maria"
-
+echo "  docker exec -it bookies-maria /bin/bash"
+echo "  export TERM=\"dumb\""
+echo "  mysql -u root -p"
+echo "  enter root"
+echo "  run SQL"
+echo ""
 echo "REMOVING THE CONTAINER AND DATA"
 echo " docker rm -f bookies-maria"
-
-docker pull mariadb:10.0.24
-
-echo "Running mariadb using docker"
-docker run --name bookies-maria -d -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 mariadb:10.0.24
-echo "waiting ..."
-sleep 20
-
-echo "Create the bookies_db, the schema is provided by the platform as well! (ansible provisions a schema)"
-mysql -u root --password=root --protocol TCP -e 'CREATE DATABASE bookies_db;'
-
-echo "Use flyway to bring the database schema to the latest version" 
-/opt/flyway-4.0/flyway -user=root -password=root -url=jdbc:mysql://localhost:3306/bookies_db -locations=filesystem:sql migrate
+echo ""
+LOCAL_IP=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f8)
+echo "DONE, connect using url: mysql://root:root@${LOCAL_IP}:3306/bookies_db"
