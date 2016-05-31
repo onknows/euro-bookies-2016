b98e40f025cf4fa24a965cd751b20eb23210ccdb
Christophe Hesters <christophe.o.hesters@cgi.com>
2016-05-31 23:16:40 +0200

    made sure everything compiles well

--- aws-setup/aws-ansiblecc-setup.sh
+++ aws-setup/aws-ansiblecc-setup.sh
@@ -7,10 +7,8 @@ echo "Installing the workshop's public key, use the private key with putty or op
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7ymBRUodtreE3trfzmhT92VqV1FlvoE60InQM1FXMBE1S30eAuUgg928LvhzStB6GSwuYzQpfMIU+WafeSj2j3azPfwUrTBlKpLnYTBc9FuC41kYUooC+nvEDUouSEgCZzZDGThtoTNB7R3bB8sc3grI5/4xi832NsHJU5ma/gd64nv2iJnOcXQcDHC60OZDJVjt1GaowypviWSv1Uchpr3holyJ7JO1WT7ZvljHqMmJp0bhnerupR1x/xRNVwNBIxOkmBZDkEcXwjypoif20sEvHr7UW0FWa0SkNF9Vs81ymkhlD5Yla94hBZhA3otQ5VX1R3zu1AE3HqculmA6V workshop-keypair" >> .ssh/authorized_keys
 echo "Installs git and the latest version of Ansible"
 sudo apt-get update
-sudo apt-get install git git-extras software-properties-common python-pip python-dev -y
-sudo pip install boto
-sudo pip install markupsafe         # markupsafe is a dependency of ansible, required!
-sudo pip install ansible==2.0.2
+sudo apt-get install git git-extras software-properties-common build-essential libffi-dev libssl-dev python-pip python-dev -y
+sudo pip install setuptools==11.3 boto markupsafe ansible==2.0.2
 
 echo "Cloning euro bookies ansible branch"
 git clone https://github.com/toefel18/euro-bookies-2016.git --single-branch --branch ansible
